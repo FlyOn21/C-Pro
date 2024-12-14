1864158a8c4d807fae230208c924a1949e8b9965
Pavlo Zhoholiev <p_zhoholiev@viyar.ua>
2024-12-14 17:14:32 +0200

    Zhoholiev Pavlo 14.12.24 - refactor custom shared ptr

--- home_work_custom_shared_ptr/custom_shared_ptr.cpp
+++ home_work_custom_shared_ptr/custom_shared_ptr.cpp
@@ -2,8 +2,49 @@
 // Created by flyon21 on 21.11.24.
 //
 #include "custom_shared_ptr.h"
-#include "unordered_set"
+#include <unordered_set>
+#include <thread>
+#include <mutex>
+#include <vector>
 
+std::mutex mtx;
+
+/**
+ * This function modifies a CustomSharedPtr<int> object in a thread-safe manner.
+ *
+ * Acquires a lock using a std::mutex to ensure only one thread modifies the
+ * shared pointer at a time. The function increases the value pointed to by
+ * the shared pointer by 10 and logs the modified value to the standard output.
+ *
+ * @param sharedPtr A CustomSharedPtr<int> object that points to an integer.
+ *                  The integer value will be modified in a thread-safe manner.
+ */
+void modifySharedPtr(CustomSharedPtr<int> sharedPtr) {
+    std::lock_guard<std::mutex> lock(mtx);
+    *sharedPtr = *sharedPtr + 10;
+    std::cout << "Thread-modified value: " << *sharedPtr << "\n";
+}
+
+/**
+ * @brief Entry point for testing and demonstrating the functionality of the CustomSharedPtr class.
+ *
+ * This function performs various operations to validate the behavior of the `CustomSharedPtr`
+ * class, including creating and managing shared pointers, demonstrating ownership semantics
+ * (such as copy, move, and destructors), and verifying thread-safety when accessing and
+ * modifying data within a shared pointer.
+ *
+ * The following behaviors are demonstrated within the `main` function:
+ *
+ * - Creation of `CustomSharedPtr` instances and managing their lifetime using reference counting.
+ * - Copy assignment behavior of shared pointers and the impact on reference count.
+ * - Move semantics for transferring ownership of the managed resource.
+ * - Overload of dereference operator (*) and equality operator (==) with shared pointers.
+ * - Use of `use_count` to track the number of references to the managed resource.
+ * - Storing `CustomSharedPtr` objects in an `std::unordered_set` using a custom hash function.
+ * - Impact of scope on the shared pointer's lifecycle and reference count.
+ * - Demonstrating thread-safe operations on the shared resource by spawning multiple threads
+ *   that concurrently modify the managed object via `modifySharedPtr`.
+ */
 int main() {
     CustomSharedPtr<int> ptr1(new int(42));
     std::cout << "ptr1 count: " << ptr1.use_count() << std::endl;
@@ -31,6 +72,22 @@ int main() {
 
     CustomSharedPtr<int> ptr4 = std::move(ptr1);
     std::cout << "After move, ptr4 count: " << ptr4.use_count() << std::endl;
+
+    // Test for threads
+    std::cout << "Initial value of ptr4: " << *ptr4 << std::endl;
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(modifySharedPtr, ptr4);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "Final value of ptr4: " << *ptr4 << std::endl;
+    std::cout << "Final use count of ptr4: " << ptr4.use_count() << std::endl;
 
     return 0;
 }
