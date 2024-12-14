daecf9ab6e40a2fb2e2afaa439603402eb992d27
Pavlo Zhoholiev <p_zhoholiev@viyar.ua>
2024-12-14 18:25:37 +0200

    Add thread safety work Zhoholiev Pavlo 14.12.24 - added mutex

--- home_work_singlton/singleton_logger.cpp
+++ home_work_singlton/singleton_logger.cpp
@@ -10,7 +10,6 @@
 #include <iomanip>
 #include <vector>
 
-
 class Logger {
 public:
     enum LogLevel {
@@ -18,11 +17,15 @@ public:
     };
 
     static Logger &getInstance() {
-        static Logger instance;
-        return instance;
+        std::lock_guard<std::mutex> lock(mtx);
+        if (instance == nullptr) {
+            instance = new Logger();
+        }
+        return *instance;
     }
 
     void log(const std::string &message, LogLevel level = INFO) {
+        std::lock_guard<std::mutex> lock(logMutex);
         std::ofstream logFile("log.txt", std::ios::app);
         std::vector<std::string> coloredLogLevel = getColoredLogLevelString(level);
         if (logFile.is_open()) {
@@ -43,9 +46,7 @@ public:
 
 private:
     Logger() = default;
-
     ~Logger() = default;
-
 
     static std::string getCurrentTime() {
         auto now = std::chrono::system_clock::now();
@@ -81,22 +82,32 @@ private:
                 return {"", ""};
         }
     }
+
+    static Logger *instance;
+    static std::mutex mtx;
+    std::mutex logMutex;
 };
+
+Logger *Logger::instance = nullptr;
+std::mutex Logger::mtx;
 
 class ConfigManager {
 public:
     static ConfigManager &getInstance() {
-        static ConfigManager instance;
-        return instance;
+        std::lock_guard<std::mutex> lock(mtx);
+        if (instance == nullptr) {
+            instance = new ConfigManager();
+        }
+        return *instance;
     }
 
     void setConfig(const std::string &key, const std::string &value) {
-        std::lock_guard<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock(configMutex);
         configData_[key] = value;
     }
 
     std::string getConfig(const std::string &key) const {
-        std::lock_guard<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock(configMutex);
         auto it = configData_.find(key);
         if (it != configData_.end()) {
             return it->second;
@@ -111,19 +122,21 @@ public:
 
 private:
     ConfigManager() = default;
-
     ~ConfigManager() = default;
 
-    mutable std::mutex mutex_;
+    static ConfigManager *instance;
+    static std::mutex mtx;
+    mutable std::mutex configMutex;
     std::map<std::string, std::string> configData_;
 };
 
-int main() {
+ConfigManager *ConfigManager::instance = nullptr;
+std::mutex ConfigManager::mtx;
 
+int main() {
     Logger::getInstance().log("This is an info message", Logger::INFO);
     Logger::getInstance().log("This is a warning message", Logger::WARNING);
     Logger::getInstance().log("This is an error message", Logger::ERROR);
-
 
     ConfigManager::getInstance().setConfig("database_host", "localhost");
     ConfigManager::getInstance().setConfig("database_port", "5432");
